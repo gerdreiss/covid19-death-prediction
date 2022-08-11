a774507a5b31672d15953aefefa53ac5e57d7569
Gerd Reiss <gerdreiss@@users.noreply.github.com>
2022-08-11 18:58:31 +0200

    showing weekly cases and next week's deaths predictions per country and year

--- modules/analysis/src/main/scala/covid19/Analysis.scala
+++ modules/analysis/src/main/scala/covid19/Analysis.scala
@@ -3,6 +3,8 @@ package covid19
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.Encoders
+import org.apache.spark.sql.Encoder
 
 object Analysis extends App {
   // Set the log level to only print errors
@@ -15,13 +17,24 @@ object Analysis extends App {
     .getOrCreate()
 
   import spark.implicits._
+
   val people = spark
     .read
+    .schema(Encoders.product[Data].schema)
     .option("header", "true")
-    .option("inferSchema", "true")
     .csv("data/CovidData.csv")
-  // .as[Data]
+    .as[Data]
 
-  people.show()
+  people
+    .select("location", "year", "weeklyCases", "nextWeeksDeaths")
+    .where(
+      ($"location" !== "World") &&
+        ($"location" !== "Europe") &&
+        ($"location" !== "Africa")
+    )
+    .groupBy("location", "year")
+    .sum("weeklyCases", "nextWeeksDeaths")
+    .sort($"location", $"year")
+    .show()
 
 }
